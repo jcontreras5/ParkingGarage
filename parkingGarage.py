1c5613ada171fa08f342ebd43c87dfa3b5336632
jcontreras5 <johnny_paze@yahoo.com>
2021-04-19 09:38:30 -0500

    Update parkingGarage.py

--- parkingGarage.py
+++ parkingGarage.py
@@ -1,35 +1,76 @@
+from IPython.display import clear_output
+
 class garage():
     def __init__(self,ticket_space,status):
-        self.ticket_space = ticket_space
+        self.ticket_space = list(range(1,ticket_space))
         self.status = status 
-""" 
-garage class(ticket,space,status):
-    ticket = int number of tickets turned into list 
-    space = int number of spaces turned into list
-    status = dict whose key=ticket number and corresponding value indicates whether in use or paid,
-"""
-#Method to take ticket
+    """ 
+    garage class(ticket,space,status):
+        ticket = int number of tickets turned into list 
+        status = dict whose key=ticket number and corresponding value indicates whether in use or paid,
+    """
+    #Method to take ticket
     def takeTicket(self):
+        clear_output
         if self.ticket_space != []:
             ticket = self.ticket_space.pop(0)
-Print(ticket)
-Print(self.ticket_space)
-Print(self.status)
             self.status[ticket]= ""
-        if self.ticket_space == []:
+            print(f'You have ticket number {ticket}')
+            print(self.ticket_space)
+            print(self.status)
+        else:
             print("unfortunately we do no have any space available")
+            
+        
 
-#method to pay for parking
+    #method to pay for parking
     def payForParking(self):
-        ticket_number = input("Welcome to the payment kiosk, what is your ticket number? ")
-        if ticket_number in status:
-            self.status[ticket]= "paid"
-        if ticket_number not in status:
+        clear_output
+        ticket_number = input("Welcome to the payment kiosk, what is your ticket number? Enter 'b' for back \n")
+        if ticket_number.lower() == "b":
+            pass
+            clear_output
+        elif int(ticket_number) in self.status:
+            self.status[ticket_number]= "paid"
+            print("Please proceed to the exit booth, your ticket has been paid.")
+        else:
             print("Please enter a valid number")
-
+        
+            
+    #method to leave garage
     def leaveGarage(self):
+        clear_output
+        leave = input("Please enter your ticket number. Enter 'b' for back \n")
+        if leave.lower() == "b":
+            pass
+            clear_output
+        elif leave in self.status:
+            if self.status[leave] == "paid":
+                self.ticket_space.append(leave)
+                sort(self.ticket_space)
+                del self.status[leave]
+                print("Thank you for parking at the Marina Towers!")
+            else:
+                print("You can't leave unless you have paid, please visit the payment kiosk") 
+        elif leave not in self.status :
+            print("Please enter a valid number")
+        
+            
 
 
 def run():
-    Marina_Towers= garage(10,{})
-        space = list in range(Marina_Towers.ticket)
+    Marina_Towers= garage(3,{})
+    while True:
+        question = (f"Welcome to Marina Towers garage.There are currently {len(Marina_Towers.ticket_space)} spaces.")
+        question += ('\n What would you like to do ?: pay/park/leave/quit \n')
+        ask = input(question)
+        if ask.lower() == "park":
+            Marina_Towers.takeTicket()
+        elif ask.lower() == "pay":
+            Marina_Towers.payForParking()
+        elif ask.lower() == "leave":
+            Marina_Towers.leaveGarage()
+        elif ask.lower() == "quit":
+            break
+
+run()
